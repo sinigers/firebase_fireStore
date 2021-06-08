01688fa4b8b1c19116c9acfaacfe9e9c215e775f
sinigers <sinigers@yahoo.com>
2021-06-08 10:48:08 +0300

    labs

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
 
 //create elment and render cafe
 function renderCafe(doc){
-let li = document.createElement('li');
-let name = document.createElement('span');
-let city = document.createElement('span');
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
 
-//link: id of element in firebase and text content
-li.setAttribute('data-id', doc.id);
-name.textContent= doc.data().name;
-city.textContent= doc.data().city;
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
 
-li.appendChild(name);
-li.appendChild(city);
+    li.appendChild(name);
+    li.appendChild(city);
 
-cafeList.appendChild(li);
+    cafeList.appendChild(li);
 }
 
 // get  data from collection and creat snapshot
@@ -24,5 +24,17 @@ db.collection('cafes').get().then((snapshot) => {
         // console.log(doc.data());
         renderCafe(doc);
     })
-}) 
+}); 
 // then()- when information is geted from db
+
+// saving data
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    db.collection('cafes').add({
+        name:form.name.value,
+        city:form.city.value
+    });
+    // clear form fealds
+    form.name.value='';
+    form.city.value='';
+})
